3bf71075d54ee4c72e54f6d40b17a7a5e08aa26b
AndSemenoff <AndSemenoff@users.noreply.github.com>
2026-01-07 20:15:34 +0000

    deploy: a3582ce078ad1cd9e9638e5f69295c23785bf422

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-7af86e67.js',
+                path_to_root + 'searchindex-c037574f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
